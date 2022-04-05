78d6ddcca0caffada6edb23e1d46354928406ea7
Igor Salviatto <igorschsal@gmail.com>
2022-04-05 14:08:39 -0300

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+export  function handlerQ(req, res) {
+  res.status(200).json({ name: 'John Doe' })
+}
+export  function handlerH(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
