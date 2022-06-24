28679a054005e13a3a3ac2126239120d88b2371e
Igor Schroter Salviatto <igorschsal@gmail.com>
2022-06-24 02:19:28 -0300

    new start

---
+++ embarcados_front/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className="bg-yellow-200 h-screen flex">
+      <div className='outline bg-white w-2/4 h-1/3 mx-20 my-10 rounded flex-auto'>
+
+      </div>
+     
+    </div>
+  )
+}
+
+export default App
