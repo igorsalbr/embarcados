4db0d10234f7400c498a845f881fb52bf3884a6e
Igor Schroter Salviatto <igorschsal@gmail.com>
2022-06-24 01:35:37 -0300

    renew

--- Front/pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export  function handlerQ(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
-export  function handlerH(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
